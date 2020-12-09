c4274ab65ebbf64469b45e7c94310a3ee663422a
Quang Truong <truongdangquang1998@gmail.com>
2020-12-10 00:05:40 +0700

    update

--- Codes/20201117/DangKyHocPhan/DangKyHocPhan/Views/UCChenLopHocPhan.xaml.cs
+++ Codes/20201117/DangKyHocPhan/DangKyHocPhan/Views/UCChenLopHocPhan.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DangKyHocPhan.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace DangKyHocPhan.Views
         public UCChenLopHocPhan()
         {
             InitializeComponent();
+            this.DataContext = new UCModelChenLopHocPhan();
         }
     }
 }

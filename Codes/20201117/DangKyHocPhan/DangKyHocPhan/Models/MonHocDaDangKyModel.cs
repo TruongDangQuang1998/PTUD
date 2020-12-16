a557a30f0ee73215e83df7dea37cd43527ef7817
Quang Truong <truongdangquang1998@gmail.com>
2020-12-16 09:27:07 +0700

    update

---
+++ Codes/20201117/DangKyHocPhan/DangKyHocPhan/Models/MonHocDaDangKyModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangKyHocPhan.Models
+{
+    public class MonHocDaDangKyModel
+    {
+        public int Id { get; set; }
+        public int IdMonHoc { get; set; }
+        public int STT { get; set; }
+        public string MaHP { get; set; }
+        public string TenMonHoc { get; set; }
+        public string GiangVien { get; set; }
+        public int SoTC { get; set; }
+        public string TrangThai { get; set; }
+        public string NhomTH { get; set; }
+    }
+}
